4006667a716f39120a95d67ddd7a693ef3fecb23
Osama Aldemeery <aldemeery@gmail.com>
2026-04-27 15:04:01 +0300

    Add an accent color.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const formatRemainingTime = (time) => `${time.hours}`.padStart(2, '0') + ':' + `${time.minutes}`.padStart(2, '0') + ':' + `${time.seconds}`.padStart(2, '0');
+const formatRemainingTime = (time) => `${time.hours}`.padStart(2, '0') + '<span class="accent">:</span>' + `${time.minutes}`.padStart(2, '0') + '<span class="accent">:</span>' + `${time.seconds}`.padStart(2, '0');
 const formatPrayerName = (prayer) => prayer.name;
 const formatPrayerTime = (prayer) => `${prayer.time.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true }).replace(/ AM| PM/, '')}`;
 
@@ -10,13 +10,13 @@ prayers.on('start', (data) => {
     document.getElementById('time').style.display = 'block';
     document.getElementById('start').remove();
 
-    document.getElementById('countdown').innerText = formatRemainingTime(data.remainingTime);
+    document.getElementById('countdown').innerHTML = formatRemainingTime(data.remainingTime);
     document.getElementById('prayer').innerText = formatPrayerName(data.nextPrayer);
     document.getElementById('time').innerText = formatPrayerTime(data.nextPrayer);
 });
 
 prayers.on('tick', (data) => {
-    document.getElementById('countdown').innerText = formatRemainingTime(data.remainingTime);
+    document.getElementById('countdown').innerHTML = formatRemainingTime(data.remainingTime);
     document.getElementById('prayer').innerText = formatPrayerName(data.nextPrayer);
     document.getElementById('time').innerText = formatPrayerTime(data.nextPrayer);
 });
